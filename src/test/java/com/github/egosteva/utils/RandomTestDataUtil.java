b801c28d9d0c75f827463d19d3663e6fa7e720d9
egosteva <elenagosteva.qa@gmail.com>
2023-05-22 23:57:32 +0300

    deleted comments

--- src/test/java/com/github/egosteva/utils/RandomTestDataUtil.java
+++ src/test/java/com/github/egosteva/utils/RandomTestDataUtil.java
@@ -9,30 +9,11 @@ public class RandomTestDataUtil {
         return faker.artist().name();
     }
 
-  //  public String getTestCaseNameUpdated() {
-  //      return faker.book().author();
-  //  }
-
     public String getTestCaseDescription() {
         return faker.address().fullAddress();
     }
 
- //   public String getTestCaseDescriptionUpdated() {
-  //      return faker.color().name();
- //   }
-
     public String getStepName() {
         return faker.book().title();
     }
-
-    //  public String getStepNameUpdated() {
-    //      return faker.book().genre();
-    // }
-
- //   String testCaseNameInitial = faker.artist().name();
- //   String testCaseNameUpdated = faker.book().author();
- //   String testCaseDescriptionInitial = faker.address().fullAddress();
- //   String testCaseDescriptionUpdated = faker.color().name();
- //   String stepNameInitial = faker.book().title();
- //   String stepNameUpdated = faker.book().genre();
 }
