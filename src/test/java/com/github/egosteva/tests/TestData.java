5de52470b76149bbe510e1cb6e038bb3b091fdef
egosteva <elenagosteva.qa@gmail.com>
2023-05-22 20:26:21 +0300

    upd allureTestopsSession

--- src/test/java/com/github/egosteva/tests/TestData.java
+++ src/test/java/com/github/egosteva/tests/TestData.java
@@ -6,5 +6,5 @@ public class TestData {
             password = "allure8",
             projectId = "2257",
             xsrfToken = "50011563-6a05-4b2e-95ca-344ef2831d85",
-            allureTestopsSession = "ca2866a5-4bb9-419d-bc97-f47a7d982e31";
+            allureTestopsSession = "b6af0c5d-6c7d-4c04-8e7d-402fff61a30e";
 }
